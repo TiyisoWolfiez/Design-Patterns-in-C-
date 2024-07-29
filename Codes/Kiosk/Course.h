5671d5e83b56bfa31b876054be6e8247723bf14a
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-07-29 19:38:49 +0200

    Update Course.h

--- Codes/Kiosk/Course.h
+++ Codes/Kiosk/Course.h
@@ -1,1 +1,28 @@
+#ifndef Course_H
+#define Course_H
 
+#include <vector>
+#include <string>
+
+
+class MenuItem;
+
+class Course {
+
+    protected:
+        std::string description;
+        int maxNumberOfItems;
+        std::vector<MenuItem*> menuItems;
+
+    public:
+        Course(const std::string& description, int maxNumberOfItems);
+        std::string getDescription();
+        bool addMenuItem(const std::string& description, float price, int stock);
+        MenuItem* getMenuItem(int index) const;
+
+        void printMenuItems();
+        void printInventory();
+        virtual void recommendBeverage() const = 0;
+        virtual ~Course();
+};
+#endif
