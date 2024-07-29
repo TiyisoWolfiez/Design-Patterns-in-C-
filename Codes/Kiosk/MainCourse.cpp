c13cb80c83e03ebfc6673fe8fec0c189344308cb
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-07-29 19:39:50 +0200

    Update MainCourse.cpp

--- Codes/Kiosk/MainCourse.cpp
+++ Codes/Kiosk/MainCourse.cpp
@@ -1,1 +1,8 @@
+#include "MainCourse.h"
+#include <iostream>
 
+MainCourse::MainCourse(int maxNumberOfItems) : Course("Main", maxNumberOfItems) {}
+
+void MainCourse::recommendBeverage() const {
+    std::cout <<"Coke"<<std::endl;
+}
