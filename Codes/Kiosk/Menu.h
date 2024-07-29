a7c6b63fb46e041756cd7682ffaa66bdd030286b
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-07-29 19:41:02 +0200

    Update Menu.h

--- Codes/Kiosk/Menu.h
+++ Codes/Kiosk/Menu.h
@@ -1,1 +1,27 @@
+#ifndef Menu_H
+#define Menu_H
 
+#include <map>
+#include <string>
+
+class Course;
+
+class Menu
+{
+public:
+    Menu();
+    bool addCourse(Course* course);
+    bool addMenuItem(const std::string& courseDescription, const std::string& description, float price, int stock);
+    void printMenu() const;
+    void inventory() const;
+    float orderItem(const std::string& courseDescription, char item);
+    bool isCoursesEmpty() const;
+    void closeShop();
+    ~Menu();
+private:
+    std::map<std::string, Course*> courses;
+
+
+};
+
+#endif
