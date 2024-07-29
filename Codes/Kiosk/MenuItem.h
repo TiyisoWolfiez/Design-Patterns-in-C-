0c6fe846e4e83f9b54576aca32f98ed4cd828f3a
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-07-29 19:41:48 +0200

    Update MenuItem.h

--- Codes/Kiosk/MenuItem.h
+++ Codes/Kiosk/MenuItem.h
@@ -1,1 +1,19 @@
+#ifndef MenuItem_H
+#define MenuItem_H
 
+#include <string>
+
+class MenuItem {
+    public:
+        MenuItem(const std::string& description, float price, int stock);
+        std::string getDescription();
+        float getPrice();
+        int getStock();
+        void reduceStock();
+
+    private:
+        std::string description;
+        float price;
+        int stock;
+};
+#endif
