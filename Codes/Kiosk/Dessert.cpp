9d20151735e722967b5314a4805690748816e034
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-07-29 19:39:09 +0200

    Update Dessert.cpp

--- Codes/Kiosk/Dessert.cpp
+++ Codes/Kiosk/Dessert.cpp
@@ -1,1 +1,9 @@
+#include "Dessert.h"
+#include <iostream>
 
+Dessert::Dessert(int maxNumberOfItems) : 
+Course("Dessert", maxNumberOfItems) {}
+
+void Dessert::recommendBeverage() const {
+    std::cout <<"Coffee"<<std::endl;
+}
