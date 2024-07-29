6d13874713ae01c3b0d57864ad25b95494d907b2
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-07-29 19:39:31 +0200

    Update Dessert.h

--- Codes/Kiosk/Dessert.h
+++ Codes/Kiosk/Dessert.h
@@ -1,1 +1,11 @@
+#ifndef Dessert_H
+#define Dessert_H
 
+#include "Course.h"
+
+class Dessert : public Course {
+    public:
+        Dessert(int maxNumberOfItems);
+        void recommendBeverage() const override;
+};
+#endif
