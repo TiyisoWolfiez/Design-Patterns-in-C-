8e6ef169dd17403417594d25a69b6338d9e876a6
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-07-29 19:42:07 +0200

    Update Starter.cpp

--- Codes/Kiosk/Starter.cpp
+++ Codes/Kiosk/Starter.cpp
@@ -1,1 +1,9 @@
+#include "Starter.h"
+#include <iostream>
 
+Starter::Starter(int maxNumberOfItems) : 
+Course("Starter", maxNumberOfItems) {}
+
+void Starter::recommendBeverage() const {
+    std::cout << "Sparkling Water" << std::endl;
+}
