b38c5f92169b66feb1b3ecd7277263a670bcfff0
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-07-29 19:42:22 +0200

    Update Starter.h

--- Codes/Kiosk/Starter.h
+++ Codes/Kiosk/Starter.h
@@ -1,1 +1,12 @@
+#ifndef Starter_H
+#define Starter_H
 
+#include "Course.h"
+
+class Starter : public Course {
+    public:
+        Starter(int maxNumberOfItems);
+        void recommendBeverage() const override;
+};
+
+#endif 
